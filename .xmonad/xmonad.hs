6ac416612e9db287795b73301f38c5c9594a48dd
Jonathan Laver <jonathan.laver@gmail.com>
2012-04-06 10:25:04 +0100

    There is now only one border when there is only one application open. Modified shortcuts for suspending

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -5,13 +5,15 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 import XMonad.Hooks.EwmhDesktops
+import XMonad.Layout.NoBorders
+import XMonad.Config.Gnome
  
 myTerminal = "/usr/bin/urxvt" 
 main = do
     xmproc <- spawnPipe "xmobar $HOME/.xmobarrc"
     xmonad $ defaultConfig
         { manageHook = manageDocks <+> manageHook defaultConfig
-        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , layoutHook = avoidStruts  $ smartBorders $ layoutHook defaultConfig
         , modMask = mod4Mask
         , handleEventHook    = fullscreenEventHook
         , borderWidth = 1
@@ -23,8 +25,8 @@ main = do
                         , ppTitle = xmobarColor "#d74b73" "" . shorten 50
                         }
         } `additionalKeys`
-        [((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+        [((mod4Mask, xK_Print), spawn "sleep 0.2; scrot -s")
         , ((0, xK_Print), spawn "scrot")
-	, ((controlMask, xK_Delete), spawn "dbus-send --system --print-reply --dest=\"org.freedesktop.ConsoleKit\" /org/freedesktop/ConsoleKit/Manager org.freedesktop.ConsoleKit.Manager.Stop")
-	, ((controlMask, xK_Pause), spawn "dbus-send --system --print-reply --dest=\"org.freedesktop.UPower\" /org/freedesktop/UPower org.freedesktop.UPower.Suspend")
+	, ((mod4Mask, xK_Delete), spawn "dbus-send --system --print-reply --dest=\"org.freedesktop.ConsoleKit\" /org/freedesktop/ConsoleKit/Manager org.freedesktop.ConsoleKit.Manager.Stop")
+	, ((mod4Mask, xK_Pause), spawn "dbus-send --system --print-reply --dest=\"org.freedesktop.UPower\" /org/freedesktop/UPower org.freedesktop.UPower.Suspend")
         ]
