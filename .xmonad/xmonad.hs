2f062ac9c8d9b4e263937e6fb796e3c4dfee8ead
Jonathan Laver <jonathan.laver@gmail.com>
2012-04-07 20:20:33 +0100

    now I can resize panes in 4 direction instead of 2

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -5,25 +5,30 @@ import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageHelpers
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
-import System.IO
 import XMonad.Layout.NoBorders
+import XMonad.Layout.ResizableTile
+import XMonad.Layout.Renamed
 import XMonad.Config.Gnome
+import System.IO
  
-myTerminal = "/usr/bin/urxvt"
-
 myManageHooks = composeAll
     [ isFullscreen --> doFullFloat
     ]
+
+myLayout = renamed [Replace "Tall"] tiled ||| renamed [Replace "Mirror Tall"] (Mirror tiled) ||| Full
+  where
+    tiled = ResizableTall 1 (3/100) (1/2) []
 
 main = do
     xmproc <- spawnPipe "xmobar $HOME/.xmobarrc"
     xmonad $ defaultConfig
         { manageHook = manageDocks <+> myManageHooks <+> manageHook defaultConfig
-        , layoutHook = avoidStruts  $ smartBorders $ layoutHook defaultConfig
+        , layoutHook = avoidStruts  $ smartBorders myLayout
+
         , modMask = mod4Mask
-        , handleEventHook    = fullscreenEventHook
+        , handleEventHook = fullscreenEventHook
         , borderWidth = 1
-	, terminal = myTerminal
+        , terminal = "/usr/bin/urxvt"
         , focusedBorderColor = "#d74b73"
         , normalBorderColor = "#3d3d3d"
         , logHook = dynamicLogWithPP xmobarPP
@@ -33,6 +38,8 @@ main = do
         } `additionalKeys`
         [((mod4Mask, xK_Print), spawn "sleep 0.2; scrot -s")
         , ((0, xK_Print), spawn "scrot")
-	, ((mod4Mask, xK_Delete), spawn "dbus-send --system --print-reply --dest=\"org.freedesktop.ConsoleKit\" /org/freedesktop/ConsoleKit/Manager org.freedesktop.ConsoleKit.Manager.Stop")
-	, ((mod4Mask, xK_Pause), spawn "dbus-send --system --print-reply --dest=\"org.freedesktop.UPower\" /org/freedesktop/UPower org.freedesktop.UPower.Suspend")
+	    , ((mod4Mask, xK_Delete), spawn "dbus-send --system --print-reply --dest=\"org.freedesktop.ConsoleKit\" /org/freedesktop/ConsoleKit/Manager org.freedesktop.ConsoleKit.Manager.Stop")
+	    , ((mod4Mask, xK_Pause), spawn "dbus-send --system --print-reply --dest=\"org.freedesktop.UPower\" /org/freedesktop/UPower org.freedesktop.UPower.Suspend")
+        , ((mod4Mask, xK_a), sendMessage MirrorShrink)
+        , ((mod4Mask, xK_z), sendMessage MirrorExpand)
         ]
